f7fd7b89e6c75fe128705898749601d25ab243fa
Nguyen Phan <nqphan212@gmail.com>
2015-07-28 15:27:12 -0400

    Done

--- app/src/main/java/com/nguyenqphan/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nguyenqphan/interactivestory/ui/StoryActivity.java
@@ -1,17 +1,40 @@
 package com.nguyenqphan.interactivestory.ui;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nguyenqphan.interactivestory.R;
+import com.nguyenqphan.interactivestory.model.Choice;
 import com.nguyenqphan.interactivestory.model.Page;
+import com.nguyenqphan.interactivestory.model.Story;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 
-
-public class StoryActivity extends ActionBarActivity {
+//AppCompatActivity is used to replace ActionBarActivity to fix error message
+// "Drawable"
+public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
+
+    private Story mStory = new Story();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +42,65 @@ public class StoryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
+        mName = intent.getStringExtra("name");
 
-        if (name == null){
-            name = "Friend";
+        if (mName == null){
+            mName = "Friend";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
 
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage(0);
     }
 
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+
+        //Drawable drawable = getResources().getDrawable(page.getImageId());
+        //is replaced by the following line code.
+        Drawable drawable = ContextCompat.getDrawable(this, mCurrentPage.getImageId());
+
+        mImageView.setImageDrawable(drawable);
+        //Add the name if placeholder included. Wont add if no placeholder
+        String pageText = mCurrentPage.getText();
+        pageText = String.format(pageText, mName);
+
+        mTextView.setText(pageText);
+
+
+        if (mCurrentPage.isFinal()) {
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } else {
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+        }
+    }
 
 }
